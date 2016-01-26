eff22008b137d0e3e22846c2162177c9007c2f3d
Fyr <fyr@tut.by>
2016-01-26 21:49:35 +0300

    Исправил ошибку текущей даты

--- app/Plugin/Core/View/Helper/PHTimeHelper.php
+++ app/Plugin/Core/View/Helper/PHTimeHelper.php
@@ -10,16 +10,18 @@ class PHTimeHelper extends TimeHelper {
 		$month = ' '.__(date('M', $date), true);
 		$year = ' '.date('Y', $date).'г.'; // (date('Y') == date('Y', $date)) 2014-09-29 20:54:38
 		$time = '';
-		if (date('d') == date('d', $date)) {
-			$day = __('Today', true);
-			$month = '';
-			$year = '';
-			$time = ', '.date('H:i', $date);
-		} elseif ((date('d') - 1) == date('d', $date)) {
-			$day = __('Yesterday', true);
-			$month = '';
-			$year = '';
-			$time = ', '.date('H:i', $date);
+		if (date('m') == date('m', $date) && date('Y') == date('Y', $date)) {
+			if (date('d') == date('d', $date)) {
+				$day = __('Today', true);
+				$month = '';
+				$year = '';
+				$time = ', ' . date('H:i', $date);
+			} elseif ((date('d') - 1) == date('d', $date)) {
+				$day = __('Yesterday', true);
+				$month = '';
+				$year = '';
+				$time = ', ' . date('H:i', $date);
+			}
 		}
 		return $day.$month.$year.$time;
 	}
