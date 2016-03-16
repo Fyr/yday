b19d9afe80ca0aeb70070b1c5f3e49f7a682ba92
Fyr <fyr@tut.by>
2016-03-16 15:39:58 +0300

    Мелкие правки

--- app/Controller/SongPacksController.php
+++ app/Controller/SongPacksController.php
@@ -21,27 +21,6 @@ class SongPacksController extends AppController {
 		$aPlans = $this->SubscrPlan->findAllByPublished(1, null, 'sorting');
 		$this->set(compact('page', 'blocks', 'aMedia', 'aSongPacks', 'aPlans'));
 
-		/*
-		$aNews = $this->News->findAllByPublished(1, null, 'News.modified DESC', 3);
-		$aCategories = $this->Category->findAllByPublished(1, null, 'Category.sorting');
-		$aProducts = $this->Product->findAllByPublished(1, null, 'Product.sorting');
-		$this->set(compact('aNews', 'aProducts', 'aCategories'));
-		*/
 	}
-/*
-	public function karaoke_systems() {
-		$page = $this->Page->findBySlug('karaoke-systems');
-		$this->set('page', $page);
 
-		$this->ProductPack = $this->loadModel('ProductPack');
-		foreach($this->aCategories as $cat_id => $category) {
-			$ids = Hash::extract($this->aProducts[$cat_id], '{n}.Product.id');
-			$conditions = array('parent_id' => $ids);
-			$order = 'price';
-			$packs = $this->ProductPack->find('first', compact('conditions', 'order'));
-			$aPrices[$cat_id] = ($packs) ? floatval($packs['ProductPack']['price']) : 0;
-		}
-		$this->set('aPrices', $aPrices);
-	}
-*/
 }
