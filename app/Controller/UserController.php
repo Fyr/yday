4515627ec967ca144dc122fd6d8c411afb7c16a1
Fyr <fyr@tut.by>
2016-03-26 12:28:04 +0300

    Сделал layout для ЛК

---
+++ app/Controller/UserController.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppController', 'Controller');
+class UserController extends AppController {
+	public $name = 'User';
+	public $layout = 'user';
+
+	public $helpers = array('Form.PHForm');
+
+	public function index() {
+
+	}
+}
